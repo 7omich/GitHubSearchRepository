d202bb8c6b9ffd0b51761dff7da5218f41b74750
nb6u7 <b6u728@gmail.com>
2017-07-12 23:59:00 +0900

    Request implementation

---
+++ GitHubAPI.swift
@@ -0,0 +1,48 @@
+//
+//  GitHubAPI.swift
+//  GitHubSearchRepository
+//
+//  Created by 岡田直道 on 2017/07/12.
+//  Copyright © 2017年 Naomichi Okada. All rights reserved.
+//
+
+import Foundation
+
+final class GitHubAPI {
+    struct SearchRepositories : GitHubRequest {
+        let keyword: String
+        
+        // GitHubRequestが要求する連想型
+        typealias Response = SearchResponse<Repository>
+        
+        var method: HTTPMethod {
+            return .get
+        }
+        
+        var path: String {
+            return "/search/repositories"
+        }
+        
+        var parameters: Any? {
+            return ["q": keyword]
+        }
+    }
+    
+    struct SearchUsers : GitHubRequest {
+        let keyword: String
+        
+        typealias Response = SearchResponse<User>
+        
+        var method: HTTPMethod {
+            return .get
+        }
+        
+        var path: String {
+            return "/search/users"
+        }
+        
+        var parameters: Any? {
+            return ["q": keyword]
+        }
+    }
+}
